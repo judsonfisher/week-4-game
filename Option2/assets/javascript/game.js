b737688ec1b4bb84036023afd0720fc6f1f18d1f
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-08-23 17:32:41 -0500

    done

--- Option2/assets/javascript/game.js
+++ Option2/assets/javascript/game.js
@@ -1,40 +1,90 @@
 // Create starting variables for game
 
-var char1HP = 0;
+var lukeSkywalker = {
+	name: "Luke Skywalker",
+	health: 100,
+	attack: 10,
+	selected: false,
+	enemy: false, 
+}
 
-var char2HP = 0;
+var darthVader = {
+	name: "Darth Vader",
+	health: 150,
+	attack: 10,
+	selected: false,
+	enemy: false,
+}
 
-var char3HP = 0;
+var obiWan = {
+	name: "Obi Wan",
+	health: 150,
+	attack: 10,
+	selected: false,
+	enemy: false,
+}
 
-var char4HP = 0;
+var hanSolo = {
+	name: "Han Solo",
+	health: 100,
+	attack: 10,
+	selected: false,
+	enemy: false,
+}
 
-var char1Selected = false;
-
-var char2Selected = false;
-
-var char3Selected = false;
-
-var char4Selected = false;
-
-var char1Enemy = false;
-
-var char2Enemy = false;
-
-var char3Enemy = false;
-
-var char4Enemy = false;
+var beginGame = false;
 
 var win = false;
 
-
 //jQuery displaying each character's individual health
+console.log(lukeSkywalker.health);
+$("#char1HP").html(lukeSkywalker.health);
+$("#char2HP").html(darthVader.health);
+$("#char3HP").html(obiWan.health);
+$("#char4HP").html(hanSolo.health);
 
 // Insert action jQuery once character is selected (lots of "if" statements)
+
+if (beginGame == false) {
+	$("#char1").click(function(){
+    $("#char1").appendTo("#charSpot");
+    $("#char2, #char3, #char4").appendTo("#enemySpot");
+	});
+
+	$("#char2").click(function(){
+    $("#char2").appendTo("#charSpot");
+	});
+
+	$("#char3").click(function(){
+    $("#char3").appendTo("#charSpot");
+    $("#char1, #char2, #char4").appendTo("#enemySpot");
+	});
+
+	$("#char4").click(function(){
+    $("#char4").appendTo("#charSpot");
+    $("#char1, #char2, #char3").appendTo("#enemySpot");
+	});
+}   
+
 // Could use an array for attack strength (goes up by an index after each hit)
 
+var userAttack = 10;
 
 // Insert action jQuery for attack function
 
+$("#attack").click(attack());
 
+function attack() {
+	// /*EnemyHP*/ = /*EnemyHP*/ - userAttack;
+	// // userAttack = userAttack + 15;
+	// /*UserHP*/ - /*EnemyAttack*/
+	$("#readout").html("Enemy attacked for " + enemyAttack + "!")
+}
 
-// Insert action jQuery for defeating opponent/game over
+// Insert action jQuery for defeating opponent/game over'
+
+// Reset
+
+$("#reset").click(function(){
+	location.reload();
+});
